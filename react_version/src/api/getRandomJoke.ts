ccec8c63cdb29567d4c27d3d7ea1361dfa27fdc2
TomaszBeben <tbeben@st.swps.edu.pl>
2022-06-16 22:06:46 +0200

    react version styling a bit

--- react_version/src/api/getRandomJoke.ts
+++ react_version/src/api/getRandomJoke.ts
@@ -4,6 +4,7 @@ export const getRandomJoke = (category: string, setError: (arg: string) => void)
     axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`)
       .then((response) => {
         alert(response.data.value)
+        console.log(response.data);
       })
       .catch((error) => {
         setError(error)
--- react_version/src/components/app/App.tsx
+++ react_version/src/components/app/App.tsx
@@ -1,9 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, FC } from "react";
 
 import { getCategories } from "../../api/getCategories";
 import { getRandomJoke } from "../../api/getRandomJoke";
 
-const App = () => {
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import CommentIcon from '@mui/icons-material/Comment';
+import IconButton from '@mui/material/IconButton';
+import ListItemButton from '@mui/material/ListItemButton'
+
+const App: FC = () => {
 
   const [categories, setCategories] = useState < string[] > ([]);
   const [error, setError] = useState < string | null > (null);
@@ -15,17 +22,27 @@ const App = () => {
   if (!categories) return null
 
   return (
-    <div>
-      {categories.map((element: string, index: number) => {
-        return (
-          <div key={index}>
-            <button onClick={() => { getRandomJoke(element, setError) }}>{element}</button>
-          </div>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <button onClick={() => setCategories([...categories].reverse())} >elo</button>
+        {categories.map((element: string, index: number) => (
+          <ListItemButton>
+            <ListItem
+              key={index}
+              disableGutters
+              secondaryAction={
+                <IconButton aria-label="comment">
+                  <CommentIcon onClick={() => { getRandomJoke(element, setError) }} />
+                </IconButton>
+              }>
 
+              <ListItemText primary={element} />
+
+            </ListItem>
+          </ListItemButton>
         )
-      })}
-      {error ? <p>{error}</p> : null}
-    </div>
+        )}
+        {error ? <p>{error}</p> : null}
+      </List>
   );
 }
 
