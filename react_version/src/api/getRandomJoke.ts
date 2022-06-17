19bfb3bb284d0a55b679d17c56f12ba42d872a90
TomaszBeben <tbeben@st.swps.edu.pl>
2022-06-17 12:01:57 +0200

    react version styling almost done

--- react_version/src/api/getRandomJoke.ts
+++ react_version/src/api/getRandomJoke.ts
@@ -4,7 +4,6 @@ export const getRandomJoke = (category: string, setError: (arg: string) => void)
     axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`)
       .then((response) => {
         alert(response.data.value)
-        console.log(response.data);
       })
       .catch((error) => {
         setError(error)
--- react_version/src/components/app/App.tsx
+++ react_version/src/components/app/App.tsx
@@ -3,15 +3,36 @@ import { useState, useEffect, FC } from "react";
 import { getCategories } from "../../api/getCategories";
 import { getRandomJoke } from "../../api/getRandomJoke";
 
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-// import CommentIcon from '@mui/icons-material/Comment';
-// import IconButton from '@mui/material/IconButton';
-import ListItemButton from '@mui/material/ListItemButton'
+import { makeStyles } from '@mui/styles';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from "@mui/material";
+import MessageIcon from '@mui/icons-material/Message';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import Box from '@mui/material/Box';
+
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Source Sans Pro", sans-serif'
+  }
+})
+
+const useStyle = makeStyles({
+  tableHead: {
+    backgroundColor: '#cccccc !important',
+    // cursor: "pointer"
+  },
+  tableBody: {
+    '&:hover': {
+      backgroundColor: '#cccccc '
+    }
+  },
+})
 
 const App: FC = () => {
-
+  const classes = useStyle()
+  const [sortingArrow, setSortingArrow] = useState<boolean>(true);
   const [categories, setCategories] = useState < string[] > ([]);
   const [error, setError] = useState < string | null > (null);
 
@@ -22,21 +43,47 @@ const App: FC = () => {
   if (!categories) return null
 
   return (
-    <div>
-      <List dense sx={{ width: '100%', maxWidth: 360}}>
-      <button onClick={() => setCategories([...categories].reverse())} >elo</button>
-      {categories.map((element: string, index: number) => (
-        <ListItem
-          key={index}
-          disableGutters>
-          <ListItemButton onClick={() => { getRandomJoke(element, setError) }}>
-            <ListItemText primary={element}  />
-          </ListItemButton>
-        </ListItem>
-      ))}
-      {error ? <p>{error}</p> : null}
-    </List>
-    </div>
+    <ThemeProvider theme={theme}>
+      <TableContainer component={Paper} sx={{ maxHeight: '400px', maxWidth: '700px' }}>
+        <Table aria-label='simple table' stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell 
+              onClick={()=>{
+                setSortingArrow(sortingArrow ? false : true);
+                setCategories([...categories].reverse());
+              } }
+              className={classes.tableHead}>
+                <Box
+                  display="flex"
+                  alignSelf="center"
+                  justifySelf="center"
+                >
+                  Categories
+                  {sortingArrow ? <ArrowDownwardIcon/> : <ArrowUpwardIcon/>}
+                </Box>
+              </TableCell>
+              <TableCell className={classes.tableHead} align='center'>Random Joke</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* <button onClick={() => setCategories([...categories].reverse())} >elo</button> */}
+            {categories.map((element: string, index: number) => (
+              <TableRow className={classes.tableBody} key={index}>
+                <TableCell
+                  onClick={() => { getRandomJoke(element, setError) }}>
+                  {element}
+                </TableCell>
+                <TableCell align='center'>
+                  <MessageIcon />
+                </TableCell>
+              </TableRow>
+            ))}
+            {error ? <p>{error}</p> : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </ThemeProvider>
   );
 }
 
