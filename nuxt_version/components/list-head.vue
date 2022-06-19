480f8949fa5a66544830f6a144c8e0d059b1654e
TomaszBeben <tbeben@st.swps.edu.pl>
2022-06-19 14:03:30 +0200

    nuxt version simple tab

---
+++ nuxt_version/components/list-head.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-simple-table>
+      <thead sticky>
+        <tr>
+          <th>{{ header.category }}</th>
+          <th>{{ header.random }}</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr
+            v-for="joke in jokes"
+            :key="joke"
+          >
+            <td>{{ joke }}</td>
+            <td @click="getJoke(joke)">{{joke}}</td>
+          </tr>
+      </tbody>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+    name: 'ListHead',
+    data() {
+        return ({
+            header: {
+                category: "Category",
+                random: "Random Joke",
+            },
+            jokes:[
+              'jeden',
+              'dwa',
+            ]
+
+
+        });
+    },
+    methods:{
+      getJoke(joke){
+        alert(joke)
+      }
+    },
+}
+</script>
